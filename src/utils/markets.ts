591884d44bcebae07702b0b7edb05b48b8db302d
Tolui <tobi418@gmail.com>
2021-05-25 20:43:30 +0800

    price

--- src/utils/markets.ts
+++ src/utils/markets.ts
@@ -12,7 +12,20 @@ interface Markets {
 
 export const serumMarkets = (() => {
   const m: Markets = {};
-  MARKETS.forEach((market) => {
+  //dex.mn implementation
+  const quest = {
+    address: new PublicKey("7QwEMFeKS8mPACndc9EzpgoqKbQhpBm1N4JCtzjGEyR7"),
+    name: "QUEST/USDT",
+    programId:new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin"),
+    deprecated:false,
+    quoteLabel:"USDT",
+    baseLabel:"QUEST"
+  }
+  const NEWMARKETS = JSON.parse(JSON.stringify(MARKETS));
+  NEWMARKETS.push(quest);
+  NEWMARKETS.forEach((market) => {
+  //dex.mn implementation
+    //MARKETS.forEach((market) => {
     const coin = market.name.split('/')[0];
     if (m[coin]) {
       // Only override a market if it's not deprecated	.
@@ -47,15 +60,26 @@ class PriceStore {
         return;
       }
       if (this.cache[marketName] === undefined) {
-        fetch(`https://serum-api.bonfida.com/orderbooks/${marketName}`).then(
+        //dex.mn implementation
+        let reqUrl = "https://serum-api.bonfida.com/orderbooks/";
+        if(marketName === 'QUESTUSDT'){
+          reqUrl = "https://api.dex.mn/orderbooks/";
+        }
+        fetch(reqUrl+marketName).then(
+        //dex.mn implementation
+        //fetch(`https://serum-api.bonfida.com/orderbooks/${marketName}`).then(
           (resp) => {
             resp.json().then((resp) => {
+              if(marketName === 'QUESTUSDT'){
+                console.log(resp)
+              }
               if (resp.data.asks === null || resp.data.bids === null) {
                 resolve(undefined);
               } else if (
                 resp.data.asks.length === 0 &&
                 resp.data.bids.length === 0
               ) {
+                console.log("here2")
                 resolve(undefined);
               } else if (resp.data.asks.length === 0) {
                 resolve(resp.data.bids[0].price);
